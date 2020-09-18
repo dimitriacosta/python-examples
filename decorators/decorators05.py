27a472171f9e2a9a5e07f446247636202655e4b1
Dimitri Acosta <dimitri316@gmail.com>
2020-09-18 00:25:35 -0600

    Simple decorators

---
+++ decorators/decorators05.py
@@ -0,0 +1,10 @@
+from decorators import debug
+
+@debug
+def make_greeting(name, age=None):
+    if age is None:
+        return f"Howdy {name}"
+    else:
+        return f"Whoa {name}! {age} already, you are growing up!"
+
+print(make_greeting("Dimitri", 36))
